76581e0af322c8e788292cd2d2ac3ecc736d2276
EbulfezSadigov <7ag0he8@code.edu.az>
2022-04-11 06:38:53 +0400

    Tasks

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Xml;
+using System.Xml.Serialization;
+using Xml2CSharp;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            string response = client.GetStringAsync("https://www.cbar.az/currencies/08.04.2022.xml").Result;
+            StringReader stringReader = new StringReader(response);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ValCurs));
+            Valute valCurs = (Valute)xmlSerializer.Deserialize(stringReader);
+            string Json = JsonConvert.SerializeObject(valCurs);
+            StreamWriter Data = new StreamWriter(@"C:\Users\test\source\repos\Task 1\Task 1\Files\main.json");
+            Data.Write(Json);
+            Data.Close();
+
+        }
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using Task2.Models;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+            string str = httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/posts").Result;
+            List<Posts> posts = JsonConvert.DeserializeObject<List<Posts>>(str);
+            foreach (Posts item in posts)
+            {
+                Console.WriteLine(item.title);
+            }
+        }
+    }
+}
